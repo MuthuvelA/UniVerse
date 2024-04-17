0300756d9544d678a73468ab6506d87e757c7b38
Pra_Kas <prakash63690@gmail.com>
2024-04-17 13:58:04 +0530

    ADDED .ENV

---
+++ lib/services/encryption.dart
@@ -0,0 +1,34 @@
+import 'package:encrypt/encrypt.dart';
+import 'package:universe/component/utils/credentials.dart';
+
+String encryptData(String data) {
+  final keyString = userCredentials.get_key();
+  // print('Key from dotenv: $keyString');
+
+  final key = Key.fromUtf8(keyString!);
+  final iv = IV.fromLength(16);
+
+  final encrypter = Encrypter(AES(key));
+
+  final encrypted = encrypter.encrypt(data, iv: iv);
+
+  print(encrypted.base64);
+  return encrypted.base64;
+}
+
+
+String decryptData(String data) {
+  final keyString = userCredentials.get_key();
+  // print('Key from dotenv: $keyString');
+
+  final key = Key.fromUtf8(keyString!);
+  final iv = IV.fromLength(16);
+
+  final encrypter = Encrypter(AES(key));
+
+  final encrypted = Encrypted.fromBase64(data);
+  final decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+  return decrypted;
+}
+
